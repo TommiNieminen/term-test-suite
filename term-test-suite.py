1d4ea07107e5bae43034f661188c002be76d6c74
niemi <nieminentommi@hotmail.com>
2022-10-03 14:04:05 +0300

    added sp file creation for scoring alternatives, changed annotation method to be more explicit

--- term-test-suite.py
+++ term-test-suite.py
@@ -1,11 +1,13 @@
 import argparse
 import itertools
-import re
+import regex
+import sys
+
 import sentencepiece as spm
 from itertools import chain, combinations
 import stanza
 
-term_tag_regex = re.compile(r'<term[^>]+tgt="(?P<tgt_term>[^"]+)"[^>]*>(?P<src_term>[^<]+)</term>')
+term_tag_regex = regex.compile(r'<term[^>]+tgt="(?P<tgt_term>[^"]+)"[^>]*>(?P<src_term>[^<]+)</term>')
 
 class TermAndTranslations:
 
@@ -13,8 +15,8 @@ class TermAndTranslations:
         self.source_string = source_string
         self.target_string = target_string
         self.term_index = term_index
-        self.source_term = source_string.replace(self.term_index,"")
-        self.target_terms = [x for x in target_string.split(self.term_index) if x]
+        self.source_term = source_string.replace(self.term_index,"").replace("¤","")
+        self.target_terms = [x for x in target_string.replace("¤","").split(self.term_index) if x]
 
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
@@ -22,6 +24,7 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def generate_all_test_pairs(line_terms,source, target):
+
     term_powerset = powerset(line_terms)
     sentence_pairs = []
     for subset in term_powerset:
@@ -63,16 +66,107 @@ def generate_spm_lines(
         spm_target_model,
         term_start_tag,
         term_end_tag,
-        trans_end_tag):
+        trans_end_tag,
+        term_lemma_dict):
     source_sp_model = spm.SentencePieceProcessor(spm_source_model)
     target_sp_model = spm.SentencePieceProcessor(spm_target_model)
 
-    all_line_sources = []
+    #the tag sp needs to be reverted, so store these
+    sp_term_start_tag = " ".join(source_sp_model.encode_as_pieces(term_start_tag))
+    sp_term_end_tag = " ".join(source_sp_model.encode_as_pieces(term_end_tag))
+
+
+    all_testset_sources = []
     for line_testset in all_line_testsets:
+        testset_sources = []
         for source,target in line_testset:
-            term_tag_regex.findall(source)
+            terms = list(term_tag_regex.finditer(source))
 
+            #first mark terms in the source with the term start and end tags and spm source
+            tagged_source = source
+            for term in terms:
+                source_term = term.group("src_term").strip()
+                tagged_source_term = f'{term_start_tag} {term.group("src_term")} {term_end_tag}'
+                tagged_source = tagged_source.replace(term.group(0),tagged_source_term)
+            sp_source = " ".join(source_sp_model.encode_as_pieces(tagged_source))
+            #revert tag sp
+            sp_source = sp_source.replace(sp_term_start_tag,term_start_tag)
+            sp_source = sp_source.replace(sp_term_end_tag, term_end_tag)
 
+            #then lemmatize target terms and append them to source terms
+            for term in terms:
+                source_term = term.group("src_term").strip()
+                sp_source_term = " ".join(source_sp_model.encode_as_pieces(source_term))
+                target_term = term.group("tgt_term").strip()
+                term_lemma = term_lemma_dict[target_term]
+                sp_term_lemma = " ".join(target_sp_model.encode_as_pieces(term_lemma))
+                tagged_sp_source_term = f'{term_start_tag} {sp_source_term} {term_end_tag}'
+                soft_constraint = f'{tagged_sp_source_term} {sp_term_lemma} {trans_end_tag}'
+                sp_source = sp_source.replace(tagged_sp_source_term,soft_constraint)
+
+            target_without_terms = term_tag_regex.sub("",target)
+            sp_target = " ".join(target_sp_model.encode_as_pieces(target_without_terms))
+
+            testset_sources.append((sp_source,sp_target))
+        all_testset_sources.append(testset_sources)
+    return all_testset_sources
+
+def get_bare_stanza_lemma(stanza_token):
+    # Hash symbol is used as compound divider in the lemmas, problem is that
+    # if the compound was hyphenated, the hash overwrites the hyphen, so restore hyphen
+    # note that this will probably fail in some cases, where there are more than two parts to the
+    # compound and some are hyphenated and others not (but the effect will be small).
+    if stanza_token and stanza_token.lemma and stanza_token.text:
+        if "#" in stanza_token.lemma and "#" not in stanza_token.text:
+            if "-" in stanza_token.text:
+                return stanza_token.lemma.replace('#','-')
+            else:
+                return stanza_token.lemma.replace('#','')
+        else:
+            return stanza_token.lemma
+    else:
+        sys.stderr.write(f"Invalid stanza token {stanza_token}\n")
+        return None
+
+def add_line_term_lemmas_to_dict(line_terms,line_testset,target_stanza_nlp,term_lemma_dict):
+    # We need to analyze at least a single occurrence of each translation variant
+    # to get the lemmas
+    sents_for_analysis = []
+    all_target_terms = list(itertools.chain.from_iterable([x.target_terms for x in line_terms]))
+    for source, target in line_testset:
+        sent_added = False
+        if not all_target_terms:
+            break
+        for target_term in all_target_terms[:]:
+            if target_term in term_lemma_dict:
+                all_target_terms.remove(target_term)
+                continue
+            if target_term in target:
+                clean_target = regex.sub(r'<term[^>]+tgt="(?P<tgt_term>[^"]+)"[^>]*>(?P<src_term>[^<]+)</term>', r"\2",
+                                      target)
+                if not sent_added:
+                    sents_for_analysis.append(clean_target)
+                    sent_added = True
+                all_target_terms.remove(target_term)
+    stanza_input = "\n\n".join(sents_for_analysis)
+    stanza_sents = target_stanza_nlp(stanza_input).sentences
+
+    all_target_terms = list(itertools.chain.from_iterable([x.target_terms for x in line_terms]))
+
+    for stanza_sent in stanza_sents:
+        for target_term in all_target_terms:
+            if target_term in term_lemma_dict:
+                continue
+            term_pos_in_sent = stanza_sent.text.find(target_term)
+            if term_pos_in_sent != -1:
+                term_pos = term_pos_in_sent+stanza_sent.words[0].start_char
+                lemma_start = [x for x in stanza_sent.words if x.start_char == term_pos][0]
+                lemma_end = [x for x in stanza_sent.words if x.end_char == term_pos + len(target_term)][0]
+                term_words = []
+                for id in range(lemma_start.id, lemma_end.id + 1):
+                    term_words.append([x for x in stanza_sent.words if x.id == id][0])
+                term_lemma_string = " ".join([get_bare_stanza_lemma(x) for x in term_words])
+                term_lemma_dict[target_term] = term_lemma_string
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -82,10 +176,13 @@ if __name__ == "__main__":
                         "where terms are indicated with pre- and post-fixed ids. Example: source: source" +
                         "with 1term1 or 2another term2 TAB translation with 1term1 or 2another term2" +
                         "Target side can contain alternatives, e.g 1first alt1second alt1")
-    parser.add_argument("--spm_output_file", type=str,
+    parser.add_argument("--sp_source", type=str,
                         help="Output source file that has been segmented with the provided source spm model, " +
                         "with terms added using the term tags from the spm model and segmented with the " +
                         "target spm model")
+    parser.add_argument("--sp_target", type=str,
+                        help="Output target file that has been segmented with the provided target spm model. " +
+                        "This file can be used for scoring translations with different soft constraints.")
     parser.add_argument("--spm_source_model", type=str,
                         help="spm model for the source language.")
     parser.add_argument("--spm_target_model", type=str,
@@ -105,15 +202,18 @@ if __name__ == "__main__":
 
     all_line_testsets = []
 
-    target_stanza_nlp = stanza.Pipeline(args.target_lang, processors='tokenize,pos,lemma,depparse')
+    if args.sp_source:
+        target_stanza_nlp = stanza.Pipeline(args.target_lang, processors='tokenize,pos,lemma,depparse')
+        term_lemma_dict = {}
 
     term_lemmas = {}
 
     with open(args.annotated_file,encoding="utf8") as annotated:
         for line in annotated:
             source,target = line.split('\t')
-            source_term_spans = re.findall(r"(\b(\d).+\2\b)",source)
-            target_term_spans = re.findall(r"(\b(\d).+\2\b)",target)
+            #TODO: change the encoding scheme to deal with e.g. 1st
+            source_term_spans = regex.findall(r"(¤(\d).+\2¤)",source)
+            target_term_spans = regex.findall(r"(¤(\d).+\2¤)",target)
             term_pairs = zip(
                 sorted(source_term_spans,key=lambda x: x[1]),
                 sorted(target_term_spans,key=lambda x: x[1]))
@@ -124,42 +224,26 @@ if __name__ == "__main__":
 
             all_line_testsets.append(line_testset)
 
-            if args.spm_output_file:
-                #We need to analyze at least a single occurrence of each translation variant
-                #to get the lemmas
-                sents_for_analysis = []
-                all_target_terms = list(itertools.chain.from_iterable([x.target_terms for x in line_terms]))
-                for source,target in line_testset:
-                    sent_added = False
-                    if not all_target_terms:
-                        break
-                    for target_term in all_target_terms[:]:
-                        if target_term in target:
-                            clean_target = re.sub(r'<term[^>]+tgt="(?P<tgt_term>[^"]+)"[^>]*>(?P<src_term>[^<]+)</term>', r"\2", target)
-                            if not sent_added:
-                                sents_for_analysis.append(clean_target)
-                                sent_added = True
-                            all_target_terms.remove(target_term)
-                stanza_input = "\n\n".join(sents_for_analysis)
-                stanza_sents = target_stanza_nlp(stanza_input).sentences
-
-                all_target_terms = list(itertools.chain.from_iterable([x.target_terms for x in line_terms]))
-
-                for stanza_sent in stanza_sents:
-                    for target_term in all_target_terms:
-                        term_pos = stanza_sent.text.find(target_term)
-                        if term_pos != -1:
-                            lemma_start = [x for x in stanza_sent.words if x.start_char == term_pos]
-                            lemma_end = [x for x in stanza_sent.words if x.end_char == term_pos+len(target_term)]
+            if args.sp_source:
+                add_line_term_lemmas_to_dict(line_terms, line_testset, target_stanza_nlp,term_lemma_dict)
 
 
 
 
-        spm_testsets = generate_spm_lines(
-            all_line_testsets,
-            args.spm_source_model,
-            args.spm_target_model,
-            args.term_start_tag,
-            args.term_end_tag,
-            args.trans_end_tag
-        )
+    spm_testsets = generate_spm_lines(
+        all_line_testsets,
+        args.spm_source_model,
+        args.spm_target_model,
+        args.term_start_tag,
+        args.term_end_tag,
+        args.trans_end_tag,
+        term_lemma_dict
+    )
+
+    with \
+        open(args.sp_source,'wt',encoding="utf8") as sp_source, \
+        open(args.sp_target,'wt', encoding="utf8") as sp_target:
+        for spm_testset in spm_testsets:
+            for source,target in spm_testset:
+                sp_source.write(source + '\n')
+                sp_target.write(target + '\n')
